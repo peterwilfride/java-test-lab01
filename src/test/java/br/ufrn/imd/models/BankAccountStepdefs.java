dc081570e162d2fac997f48d2950529682531f7c
peterwilfride <cybermind545@gmail.com>
2022-08-16 10:15:11 -0300

    fix bdd test

--- src/test/java/br/ufrn/imd/models/BankAccountStepdefs.java
+++ src/test/java/br/ufrn/imd/models/BankAccountStepdefs.java
@@ -14,8 +14,13 @@ public class BankAccountStepdefs {
     private BankAccount bankDestiny;
 
     @Given("um BankAccount com saldo de R$ {double}")
-    public void um_bank_account_com_saldo_de_r$(Double double1) {
+    public void um_bank_account__com_saldo_de_r$(Double double1) {
         bankAccount = new BankAccount(123456, 123, double1);
+    }
+
+    @Given("um BankAccount de destino com saldo de R$ {double}")
+    public void um_bank_account_de_destino_com_saldo_de_r$(Double double1) {
+        bankDestiny = new BankAccount(123457, 124, double1);
     }
 
     @When("depositar R$ {double}")
@@ -23,45 +28,23 @@ public class BankAccountStepdefs {
         bankAccount.deposit(double1);
     }
 
-    @Then("o saldo do BankAccount deve ser R$ {double}")
-    public void o_saldo_deve_ser_r$(Double double1) {
-        var saldo = bankAccount.getBalance();
-        assertEquals(double1, saldo);
-    }
-
-    @Given("um BankAccount com saldo de R$ {double}")
-    public void um_conta_de_banco__com_saldo_de_r$(Double double1) {
-        bankAccount = new BankAccount(123456, 123, double1);
-    }
-
-    @When("depositar R$ {double}")
+    @When("retirar R$ {double}")
     public void retirar_r$(Double double1) {
         bankAccount.withdraw(double1);
     }
 
-    @Then("o saldo do BankAccount deve ser R$ {double}")
-    public void o_saldo_da_conta_deve_ser_r$(Double double1) {
-        var saldo = bankAccount.getBalance();
-        assertEquals(double1, saldo);
-    }
-
-    @Given("um BankAccount com saldo de R$ {double} e um banco destino com saldo de R$ {double}")
-    public void um_bank_account_com_saldo_de_r$(Double double1, Double double2) {
-        bankAccount = new BankAccount(123456, 123, double1);
-        bankDestiny = new BankAccount(123457, 124, double2);
-    }
-
-    @When("transfetir R$ {double} para o banco destino")
-    public void tranferir_r$(Double double1) {
+    @When("tranferir R$ {double} para o BankAccount de destino")
+    public void tranferir_r$_para_o_bank_account_de_destino(Double double1) {
         bankAccount.transfer(bankDestiny, double1);
     }
-
-    @Then("o saldo do BankAccount deve ser R$ {double} e o saldo do BankAccount de destino deve ser R$ {double}")
-    public void o_saldo_deve_ser_r$(Double double1, Double double2) {
+    @Then("o saldo do BankAccount deve ser R$ {double}")
+    public void o_saldo_do_bank_account_deve_ser_r$(Double double1) {
         var saldo1 = bankAccount.getBalance();
-        var saldo2 = bankDestiny.getBalance();
         assertEquals(double1, saldo1);
-        assertEquals(double2, saldo2);
     }
-
+    @Then("o saldo do BankAccount de destino deve ser R$ {double}")
+    public void o_saldo_do_bank_account_de_destino_deve_ser_r$(Double double1) {
+        var saldo1 = bankAccount.getBalance();
+        assertEquals(double1, saldo1);
+    }
 }
